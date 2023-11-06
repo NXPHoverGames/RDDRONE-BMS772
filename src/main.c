43d6257fa1f4bd8e6c2c601e3587e0a67b0e08d3
lmiles-sosub <lmiles@sosub.com.au>
2023-11-06 05:28:21 +0000

    UV recovery

--- src/main.c
+++ src/main.c
@@ -816,6 +816,7 @@ static int mainTaskFunc(int argc, char *argv[])
     struct timespec sampleTime2;
     int32_t int32tVal;
     float cellVoltage;
+    float outputVoltage;
     float floatVal;
     uint8_t amountOfCBChargeCycles = 0;
     uint16_t amountOfMissedMessages = 0;
@@ -1095,11 +1096,12 @@ static int mainTaskFunc(int argc, char *argv[])
                     }
                     if(BMSFault & BMS_CELL_UV)
                     {
+                        /*
                         cli_printfError("Cell undervoltage detected!\n");
 
                         sprintf(buffer, "%ld", BMSFault);
                         cli_printf("BMS Fault Code: %s\n", buffer);
-
+                        
                         data_getParameter(V_CELL1, &cellVoltage, NULL);
                         sprintf(buffer, "%.3f", cellVoltage);
                         cli_printf("Under Voltage Cell 1: %s\n", buffer);
@@ -1115,40 +1117,92 @@ static int mainTaskFunc(int argc, char *argv[])
                         data_getParameter(V_CELL4, &cellVoltage, NULL);
                         sprintf(buffer, "%.3f", cellVoltage);
                         cli_printf("Under Voltage Cell 4: %s\n", buffer);
+                        */
 
-                        //To prevent not being able to recover and under charged battery
-                        //Check if the charger is connected and the State is fault
-                        if (batManagement_checkOutputVoltageActive() && getMainState() == INIT)
-                        {
-                            cli_printf("setting the go into fault state to False\n");
-                            // Set the flag so the under voltage fault won't cause BMS to go into fault
-                            goIntoFaultFlag = false;   
-                        }
+                        //To prevent not being able to recover an under charged battery
+                        //We check if the charger is connected and able to charge
+                        //If so don't go into fault go into charge state
+                        
+                        //Check if the output gate is open, write it to int32Val buffer
+                        if(data_getParameter(S_OUT, &int32tVal, NULL) == NULL)
+                            {
+                               cli_printfError("main ERROR: getting S-OUT went wrong!\n");
+                               int32tVal = S_OUT_DEFAULT;
+                            } 
 
+                        //Check Current write to floatVal buffer
+                        if(data_getParameter(I_BATT, &floatVal, NULL) == NULL)
+                            {
+                               cli_printfError("main ERROR: getting I-BATT went wrong!\n");
+                               floatVal = 0;
+                            }
+
+                        // Check output volatge write to outputVoltage buffer
+                        if(data_getParameter(V_OUT, &outputVoltage, NULL) == NULL)
+                            {
+                               cli_printfError("main ERROR: getting I-BATT went wrong!\n");
+                               floatVal = 0;
+                            }
+                        
+                        
+                        //Print read values for debugging
                         if (batManagement_checkOutputVoltageActive())
                         {
-                            cli_printf("Output is active\n");
+                            cli_printf("Charger on\n");
                         }
                         else
                         {
-                            cli_printf("Output is inactive\n");
+                            cli_printf("Charger off\n");
                         }
 
-                        if (getMainState() == FAULT)
+                        if (!int32tVal)
                         {
-                            cli_printf("Mode is FAULT\n");
+                            cli_printf("Output off\n");
                         }
                         else
                         {
-                            cli_printf("Mode is not Fault\n");
-                            sprintf(buffer, "%d", getMainState());
-                            cli_printf(buffer);
+                            cli_printf("Output on\n");
                         }
 
-                        //
+                        if (floatVal > 0)
+                        {
+                            cli_printf("charging\n");
+                        }
+                        else
+                        {
+                            cli_printf("not charging\n");
+                        }
 
-                        // set the variable
-                        cellUnderVoltageDetected = true;
+                        sprintf(buffer, "%f", floatVal);
+                        cli_printf("ChargeCurrent ma: %s\n", buffer);
+
+                        sprintf(buffer,"%f",outputVoltage);
+                        cli_printf("Output Volatage: %s\n", buffer);
+                        
+
+                        //If the charger is connected and the output is off or there is a charge current and not in self test mode
+                        if ((batManagement_checkOutputVoltageActive() && !int32tVal || floatVal > 0.1) && getMainState() != SELF_TEST  )
+                        {
+                            
+                            cli_printf("setting the go into fault state to False\n");
+                            // Set the flag so the under voltage fault won't cause BMS to go into fault
+                            goIntoFaultFlag = false;
+                            
+                            //rest the fault
+                            batManagement_checkFault(&BMSFault, true); 
+
+                            //Close the gate to start charging
+                            batManagement_setGatePower(GATE_CLOSE);
+
+                            setMainState(CHARGE);
+                            
+                        }
+                        else
+                        {
+                            cli_printf("Under voltage and charger off Going into fault state \n");
+                            // set the variable
+                            cellUnderVoltageDetected = true;
+                        } 
                     }
                     if(BMSFault & BMS_UT)
                     {
